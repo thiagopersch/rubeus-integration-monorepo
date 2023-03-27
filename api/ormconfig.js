88f4dd5dadaf841b9527561ea5f1b54e0d8570b1
Tiago Persch <thiagopersch@gmail.com>
2023-03-27 17:54:02 -0300

    Modified ormconfig.js

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -24,7 +24,7 @@ module.exports = {
   host: process.env.POSTGRES_HOST ?? "127.0.0.1",
   port: process.env.POSTGRES_PORT ?? 5432,
   username: process.env.POSTGRES_USERNAME ?? "postgres",
-  password: process.env.POSTGRES_PASSWORD ?? "ieadma",
-  database: process.env.POSTGRES_DATABASE ?? "ieadma",
+  password: process.env.POSTGRES_PASSWORD ?? "rubeusintegration",
+  database: process.env.POSTGRES_DATABASE ?? "rubeusintegration",
   ...entities_config,
 };
