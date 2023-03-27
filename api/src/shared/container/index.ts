0a674044194a5f4100e07a806deb3697836d434d
Tiago Persch <thiagopersch@gmail.com>
2023-03-27 17:50:25 -0300

    Added style monorepo

---
+++ api/src/shared/container/index.ts
@@ -0,0 +1,11 @@
+import { container } from 'tsyringe';
+
+// import IStudentClassCallDetailsTotalRepository from '@modules/eEBD/repositories/IStudentClassCallDetailsTotalRepository';
+// import StudentClassCallDetailsTotalRepository from '@modules/eEBD/infra/typeorm/repositories/StudentClassCallDetailsTotalRepository';
+
+// container.registerSingleton<IUsersRepository>(
+//   'UsersRepository',
+//   UsersRepository,
+// );
+
+// container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
---
+++ api/src/shared/decorators/privateRoute.ts
@@ -0,0 +1,38 @@
+// import ensureAuthenticated from '@modules/gusers/infra/http/middlewares/ensureAuthenticated';
+// import ensureModuleAccess from '@modules/core/infra/http/middlewares/ensureModuleAccess';
+
+// type PrivateRouteProps = {
+//   MODULE: string;
+//   RULE?: 'READ' | 'WRITE';
+// };
+
+// function privateRoute(params?: PrivateRouteProps) {
+//   return (
+//     target: any,
+//     propertyKey: string,
+//     propertyDescriptor: PropertyDescriptor,
+//   ): PropertyDescriptor => {
+//     const originalMethod = propertyDescriptor.value;
+
+//     // eslint-disable-next-line no-param-reassign
+//     propertyDescriptor.value = async function newMethod(...args: any) {
+//       const { profileId } = await ensureAuthenticated.apply(this, args);
+
+//       if (params) {
+//         const { MODULE, RULE } = params;
+
+//         await ensureModuleAccess({
+//           MODULE,
+//           RULE,
+//           GUSERPROFILE_ID: profileId,
+//         });
+//       }
+
+//       return originalMethod.apply(this, args);
+//     };
+
+//     return propertyDescriptor;
+//   };
+// }
+
+// export default privateRoute;
