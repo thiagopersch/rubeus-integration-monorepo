4dc6fb0c2b051b71c3b70db132b328cb3bb8a1d7
Tiago Persch <thiagopersch@gmail.com>
2023-04-03 18:24:47 -0400

    Modified generators module

---
+++ api/generators/plopfile.js
@@ -0,0 +1,20 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const fs = require('fs');
+
+const TEMPLATES_PATH = path.resolve(__dirname, 'templates');
+const templates = fs.readdirSync(TEMPLATES_PATH);
+
+module.exports = plop => {
+  templates.forEach(template => {
+    const templatePath = path.join(TEMPLATES_PATH, template);
+    const templatePlopFile = path.join(templatePath, 'plopfile.js');
+
+    if (fs.existsSync(templatePlopFile)) {
+      const { name, generator } = require(templatePlopFile);
+      plop.setGenerator(name, generator);
+    }
+  });
+};
--- api/generators/templates/module/plopfile.js
+++ api/generators/templates/module/plopfile.js
@@ -1,39 +1,42 @@
-const path = require("path");
-const fs = require("fs");
-const Handlebars = require("handlebars");
+/* eslint-disable prefer-template */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const fs = require('fs');
+const Handlebars = require('handlebars');
 
-Handlebars.registerHelper("camelCase", function (str) {
+Handlebars.registerHelper('camelCase', function (str) {
   return str
     .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
       return index === 0 ? word.toLowerCase() : word.toUpperCase();
     })
-    .replace(/\s+/g, "");
+    .replace(/\s+/g, '');
 });
 
-const moduleDir = "../../../src/modules/{{camelCase name}}";
+const moduleDir = '../../../src/modules/{{camelCase name}}';
 
 const directories = [
-  "dtos/",
-  "infra/",
-  "infra/http",
-  "infra/http/controllers",
-  "infra/http/routes",
-  "infra/typeorm",
-  "infra/typeorm/entities",
-  "infra/typeorm/repositories",
-  "repositories",
-  "repositories/fakes",
-  "services",
+  'dtos/',
+  'infra/',
+  'infra/http',
+  'infra/http/controllers',
+  'infra/http/routes',
+  'infra/typeorm',
+  'infra/typeorm/entities',
+  'infra/typeorm/repositories',
+  'repositories',
+  'repositories/fakes',
+  'services',
 ];
 
 module.exports = {
-  name: "module",
+  name: 'module',
   generator: {
     prompts: [
       {
-        type: "input",
-        name: "name",
-        message: "What is the name of the module?",
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the module?',
       },
     ],
     actions: [
@@ -42,24 +45,24 @@ module.exports = {
         const moduleDirCompiled = template(answers);
         const modulepath = path.resolve(
           __dirname,
-          ...moduleDirCompiled.split("/")
+          ...moduleDirCompiled.split('/'),
         );
 
         if (fs.existsSync(modulepath)) {
-          console.log("This module already exists!");
+          console.log('This module already exists!');
           return;
         }
 
         fs.mkdirSync(modulepath);
-        console.log("+ " + modulepath);
+        console.log('+ ' + modulepath);
 
-        directories.forEach((directory) => {
-          const dirPath = path.join(modulepath, ...directory.split("/"));
+        directories.forEach(directory => {
+          const dirPath = path.join(modulepath, ...directory.split('/'));
           fs.mkdirSync(dirPath);
-          console.log("+ " + dirPath);
+          console.log('+ ' + dirPath);
         });
 
-        console.log("Module created successfully!!");
+        console.log('Module created successfully!!');
       },
     ],
   },
