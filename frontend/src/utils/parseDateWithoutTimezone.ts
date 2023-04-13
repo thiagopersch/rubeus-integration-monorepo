527602be8b673fe6a6b630e841d9d9969af868e8
Tiago Persch <thiagopersch@gmail.com>
2023-04-13 17:57:45 -0400

    Deleted archive utils/parseDateWithouttimezone.ts

--- frontend/src/utils/parseDateWithoutTimezone.ts
+++
@@ -1,6 +0,0 @@
-export const parseDateWithoutTimezone = (date: string | Date) => {
-  if (date instanceof Date) return date;
-
-  const normalizedDate = date.replace(/z$/i, "");
-  return normalizedDate;
-};
